070f459af3fab459a5e345c438255aec901e1deb
Iryna Zubko <irinazubko.ntu@gmail.com>
2022-10-22 20:17:20 +0300

    Update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,8 @@ const inputEl = document.querySelector('#validation-input[data-length="6"]');
 inputEl.addEventListener('blur', handleInputBlur);
 
 function handleInputBlur() {
-    if (inputEl.value.length < inputEl.dataset.length) {
-        inputEl.classList.remove('valid');
+    if (inputEl.value.length !== Number(inputEl.dataset.length)) {
         inputEl.classList.add('invalid');
-    } else {
-        inputEl.classList.remove('invalid');
-        inputEl.classList.add('valid');
     }
+    inputEl.classList.add('valid');
 }
